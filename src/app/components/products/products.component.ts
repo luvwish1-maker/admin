55b535c4ecdd7d9f7152af3b9dd55198f5b19e4e
cuddleminddev <cuddleminddev@gmail.com>
2025-08-16 11:42:42 +0530

    products list basic

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,11 +1,85 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-
+  products = [
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+    {
+      image: '/product.png',
+      name: 'Period kit + Patch Relief Combo',
+      price: '800.40',
+      summary: 'Each kit includes five sanitary pads, a 25ml sanitizer, and five toilet sheets for hygiene on the go.',
+      sales: '1269',
+      remaining: '1269'
+    },
+  ]
 }
