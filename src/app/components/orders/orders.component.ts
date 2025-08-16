e036024ba8cb2d10c015e5b1499e414ff911695b
cuddleminddev <cuddleminddev@gmail.com>
2025-08-16 16:50:09 +0530

    order list basic

--- src/app/components/orders/orders.component.ts
+++ src/app/components/orders/orders.component.ts
@@ -1,11 +1,93 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './orders.component.html',
   styleUrl: './orders.component.css'
 })
 export class OrdersComponent {
-
+  orders = [
+    {
+      id: 1,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+    {
+      id: 2,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+    {
+      id: 3,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+    {
+      id: 4,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+    {
+      id: 5,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+    {
+      id: 6,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+    {
+      id: 7,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+    {
+      id: 8,
+      product: 'Period kit + Pain relief patch Combo',
+      orderID: 'order1',
+      date: '01/02/2025',
+      customerName: 'John Doe',
+      location: 'Ernakulam, Kerala',
+      status: 'Delivered',
+      amount: '500'
+    },
+  ]
 }
