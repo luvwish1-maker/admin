927e81c757e05b6926e3896c93721f36204a887c
cuddleminddev <cuddleminddev@gmail.com>
2025-09-03 11:06:49 +0530

    product details

--- src/app/components/product-detail/product-detail.component.ts
+++ src/app/components/product-detail/product-detail.component.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { ProductsService } from '../services/products/products.service';
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product: any = null;
+  loading = false;
 
+  constructor(
+    private route: ActivatedRoute,
+    private service: ProductsService,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.fetchProduct(productId);
+    }
+  }
+
+  fetchProduct(id: string) {
+    this.loading = true;
+    this.service.getProductByID(id).subscribe({
+      next: (res:any) => {
+        this.product = res.data;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  onEdit() {
+    if (this.product?.id) {
+      this.router.navigate(['/products/edit', this.product.id]);
+    }
+  }
 }
